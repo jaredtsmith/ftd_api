5e2c9f73a5bf5f21118b77f047d69c73559d4e52
Jared Smith <jarsmith@cisco.com>
2020-04-15 20:10:42 -0500

    reving version for new local filter ability

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ftd_api",
-    version="0.0.7",
+    version="0.0.8",
     author="Jared T. Smith",
     author_email="jarsmith@cisco.com",
     description="Useful tooling for the Firepower Threat Defense on-box REST API",
